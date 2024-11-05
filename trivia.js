c66fb19099f86a26f757990ae1ea27b1b02abf40
CarlosJoseAcosta <116382349+CarlosJoseAcosta@users.noreply.github.com>
2024-11-05 15:59:58 +0000

    Add files via upload

---
+++ trivia.js
@@ -0,0 +1,38 @@
+let puntos = 0;
+function respuesta(){
+    if(document.querySelector("#res3").checked){
+        puntos ++;
+    }else if(document.querySelector("#res1")){
+        
+    }
+    if(document.querySelector("#res5").cheked){
+        puntos ++;
+    }
+    if(document.querySelector("#res9").checked){
+        puntos ++;        
+    }
+    if(document.querySelector("#res10").checked){
+        puntos ++;        
+    }
+    if(document.querySelector("#res13").checked){
+        puntos ++;        
+    }
+    if(document.querySelector("#res17").checked){
+        puntos ++;        
+    }
+    if(document.querySelector("#res21").checked){
+        puntos ++;        
+    }
+    if(document.querySelector("#res22").checked){
+        puntos ++;        
+    }
+    if(document.querySelector("#res27").checked){
+        puntos ++;        
+    }
+    if(document.querySelector("#res28").checked){
+        puntos ++;        
+    }
+    document.querySelector()
+}
+
+document.querySelector("#fin").addEventListener("click", respuesta);
